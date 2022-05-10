852c5bfd0bfc55f9f210ccd4e137afba4da4a8c2
mahbuba <mahbubaislam7010@gmail.com>
2022-05-10 13:47:42 -0400

    Blogs done

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -26,6 +26,9 @@ const Header = () => {
                         <Nav className="me-auto">
                             <Nav.Link href="home#InventoryItems">InventoryItems</Nav.Link>
                             <Nav.Link href="home#experts">Experts</Nav.Link>
+                            <Nav.Link as={Link} to="Blogs">
+                                Blogs
+                            </Nav.Link>
                             <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
                                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                                 <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
